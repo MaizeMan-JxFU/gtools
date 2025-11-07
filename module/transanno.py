27b1ad5956d6ac9d33421971f06b094119b2b38f
Jingxian <827888724@qq.com>
2025-11-07 17:21:54 +0800

    Fixed some bugs

--- module/transanno.py
+++ module/transanno.py
@@ -15,6 +15,7 @@ descItem = "description"
 print('* Annotating...')
 df_filter = pd.read_csv(filterfile,sep='\t').set_index(['#CHROM','POS'])
 df_trans = pd.read_csv(idtransfile,sep='\t',header=None).set_index([0,1])
+df_filter = df_filter.loc[df_filter.index.isin(df_trans.index)]
 anno = readanno(annofile,descItem) # After treating: anno 0-chr,1-start,2-end,3-geneID,4-description1,5-description2
 desc = list(map(lambda x:anno.loc[(anno[0]==x[0])&(anno[1]<=x[1])&(anno[2]>=x[1])], df_trans.loc[df_filter.index].values))
 df_filter['trans_desc'] = list(map(lambda x:f'''{x.iloc[0,3]};{x.iloc[0,4]};{x.iloc[0,5]}''' if not x.empty else 'NA;NA;NA', desc))
