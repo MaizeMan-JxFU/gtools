84dee2ea8514e59fd3bdce1e6f04266c1aea0707
Jingxian <827888724@qq.com>
2025-11-08 23:20:18 +0800

    Fixed some bugs

--- install.bat
+++ install.bat
@@ -1,4 +1,5 @@
-pip install uv
+python -m pip config set global.index-url https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+python -m pip install uv
 python -m uv venv --clear
 python -m uv sync
 copy doc\windows.bat gtools.bat
--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-pip install uv
+python -m pip config set global.index-url https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple # set mirror
+python -m pip install --upgrade pip
+python -m pip install uv
 python -m uv venv --clear
 python -m uv sync
 cp doc/unix.sh gtools
