9a75344c90ff46eb2edf872f9f837832710645f4
Jingxian <827888724@qq.com>
2025-11-10 17:13:52 +0800

    在安装脚本中添加了设置 Python 安装镜像的环境变量

--- install.bat
+++ install.bat
@@ -1,5 +1,6 @@
 python -m pip config set global.index-url https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
 python -m pip install uv
+set UV_PYTHON_INSTALL_MIRROR=https://repo.huaweicloud.com/python/
 python -m uv venv --clear
 python -m uv sync
 copy doc\windows.bat gtools.bat
