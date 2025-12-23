ef7471742b25a59ca06199a1dc758da52e4f1cbd
Jingxian <827888724@qq.com>
2025-12-24 01:07:26 +0800

    Fixed some bugs

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ pip install uv
 uv venv --clear
 uv sync
 uv pip install -e ./ext/glm_rs
+uv pip install -e ./ext/lmm_rs
 uv pip install -e ./ext/geno2phylip
+uv pip install -e ./ext/gfreader_rs
 cp ./doc/unix.sh jx
 ./jx -h
 echo "Recommend: Add $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) to PATH"
